30149a380497121ab9d2778ba1f8cf174355a0e3
xushuhui <474497097@qq.com>
2019-09-25 18:37:28 +0800

    add:merge sort

--- merge_sort.go
+++ merge_sort.go
@@ -6,3 +6,20 @@
  * 博客: https://www.phpst.cn
  */
 package main
+
+func MergeSort(arr []int) {
+	n := len(arr)
+	sort(arr, 0, n-1)
+}
+func sort(arr []int, left, right int) {
+	if left >= right {
+		return
+	}
+	mid := (left + right) / 2
+	sort(arr, left, mid)
+	sort(arr, mid+1, right)
+	merge(arr, left, mid, right)
+}
+func merge(arr []int, left, mid, right int) {
+
+}
