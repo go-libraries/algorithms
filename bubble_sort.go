2691d6668ea9ec7519accbe452b6419802a16300
xushuhui <474497097@qq.com>
2019-09-27 15:16:07 +0800

    update

--- bubble_sort.go
+++ bubble_sort.go
@@ -16,5 +16,21 @@ func BubbleSort(arr []int) {
 			break
 		}
 	}
-
+}
+func BubbleSortAdvance(arr []int) {
+	n := len(arr)
+	var newn int
+	for {
+		newn = 0
+		for i := 1; i < n; i++ {
+			if arr[i-1] > arr[i] {
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+				newn = i
+			}
+		}
+		n = newn
+		if newn <= 0 {
+			break
+		}
+	}
 }
--- bubble_sort_test.go
+++ bubble_sort_test.go
@@ -3,8 +3,10 @@ package main
 import "testing"
 
 func TestBubbleSort(t *testing.T) {
-	n := 1000000
+	n := 100000
 	s1 := generateRandomArray(n, 0, n)
+	var s2 = make([]int, n)
+	copy(s2, s1)
 	timeSpent("BubbleSort", BubbleSort, s1)
-
+	timeSpent("BubbleSort", BubbleSortAdvance, s2)
 }
